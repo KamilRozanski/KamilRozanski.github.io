68231a11332bc3dfc28ad22cd5d5a61056bb69c4
Kamil Rozanski <motomc1@gmail.com>
2019-09-16 23:18:41 +0100

    API RESP in progres

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,59 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
+import { listenerCount } from "cluster";
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// // service worker registration - remove if you're not going to use it
+
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
 
 // place your code below
 
 
-console.log(`Hello world!`);
+console.log(`Code review, Is never enoug 👋`);
+
+fetch('https://api.github.com/users/KamilRozanski/repos?sort=updated')
+.then(resp => resp.json()) 
+.then(resp => {
+  const repos = resp;
+  for ( const repo of repos) {
+    const {descriptions, homepage, html_url, name} = repo;
+    
+    list.innerHTML += `
+    <li class="project-list__item">
+    <div class="project-list-wrapper">
+      <img class="project-list__logo" src="assets/img/Github-Icon.png" alt="github-logo">
+      <h4 class="project-list__title">${name}</h4>
+      <p class="project-list__description">${descriptions}
+        </p>
+    </div>
+    <section class="project-footer">
+      <div class="project-footer-wrapper">
+      <a class="project-footer__link" href="#"> <span class="project-footer__span">Demo</span> </a>
+      <a class="project-footer__link" href="#"><span class="project-footer__span project-footer__span--code">Github</span> </a>
+    </div>
+    </section>
+  </li>`
 
 
+    console.log (name);
+  }
+
+})
+  
+
+.catch(err => {
+  console.log ('error');
+})
+
+
+console.log ('dupa)';
